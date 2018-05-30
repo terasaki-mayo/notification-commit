39bc58337a8d9a5c1a95594a9e4ae6a972861adf
maybou <uminokaze521@gmail.com>
2018-05-31 08:36:38 +0900

    become class

--- TwitterAuth.php
+++ TwitterAuth.php
@@ -3,18 +3,27 @@ require_once __DIR__.'/vendor/autoload.php';
 use Dotenv\Dotenv;
 use Abraham\TwitterOAuth\TwitterOAuth;
 
-$dotenv = new Dotenv(__DIR__);
-$dotenv->load(); //.envが無いとエラーになる
+class TwitterAuth {
+    public function __construct() {
+        $dotenv = new Dotenv(__DIR__);
+        $dotenv->load(); //.envが無いとエラーになる
 
-$consumerKey = getenv('CONSUMER_KEY');
-$consumerSecret = getenv('CONSUMER_SECRET');
-$accessToken = getenv('ACCESS_TOKEN');
-$accessTokenSecret = getenv('ACCESS_TOKEN_SECRET');
+        $consumerKey = getenv('CONSUMER_KEY');
+        $consumerSecret = getenv('CONSUMER_SECRET');
+        $accessToken = getenv('ACCESS_TOKEN');
+        $accessTokenSecret = getenv('ACCESS_TOKEN_SECRET');
 
-$connection = new TwitterOAuth($consumerKey, $consumerSecret, $accessToken, $accessTokenSecret);
+        $connection = new TwitterOAuth($consumerKey, $consumerSecret, $accessToken, $accessTokenSecret);
 
-//認証コードの下にこれ追加するだけ！
-$result = $connection->post("statuses/update", array("status" => ""));
+    }
 
-//返ってきた内容を確認してみる
-var_dump($result);
+    public function Post(){
+
+        $result = $connection->post("statuses/update", array("status" => ""));
+        var_dump($result);
+    }
+
+
+
+
+}
