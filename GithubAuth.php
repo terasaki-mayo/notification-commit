5deb4933cde2201eb0ae9bb32dcee51a6a61aa57
maybou <uminokaze521@gmail.com>
2018-06-02 14:56:57 +0900

    use github api

--- GithubAuth.php
+++ GithubAuth.php
@@ -4,13 +4,47 @@ use Dotenv\Dotenv;
 
 class GithubAuth {
 
-    private $connection;
+    private $accessToken;
 
     public function __construct() {
         $dotenv = new Dotenv(__DIR__);
         $dotenv->load(); //.envが無いとエラーになる
 
-        $accessToken = getenv('GITHUB_ACCESS_TOKEN');
+        $this->accessToken = getenv('GITHUB_ACCESS_TOKEN');
+        var_dump(getenv('GITHUB_ACCESS_TOKEN'));
+    }
 
+    public function GetInfo() {
+        $query = <<<EOT
+        {
+            user(login: "Terasaki-Mayo") {
+                repositories(first: 10) {
+                    nodes {
+                        name
+                        url
+                    }
+                }
+            }
+        }
+EOT;
+
+        $options = [
+            'http' => [
+                'method' => 'POST',
+                'header' => [
+                    'User-Agent: My User Agent',
+                    'Authorization: bearer '.$this->accessToken,
+                    'Content-type: application/json; charset=UTF-8',
+                ],
+                'content' => json_encode(['query' => $query]),
+            ],
+        ];
+        $context = stream_context_create($options);
+        $contents = file_get_contents('https://api.github.com/graphql', false, $context);
+        var_dump(json_decode($contents));
+    
     }
 }
+
+$github = new GithubAuth();
+$github->GetInfo();
