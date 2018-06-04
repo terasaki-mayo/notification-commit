c79346f6a63b3f86b03486c7079cdb2ccab9216c
maybou <uminokaze521@gmail.com>
2018-06-04 23:40:49 +0900

    not work

--- GithubAuth.php
+++ GithubAuth.php
@@ -26,6 +26,7 @@ class GithubAuth {
                     edges {
                         node {
                             name
+                            languages
                         }
                     }
                 }
